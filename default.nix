2845e6c3a46a5cecb71dff94d4da946bea60ce75
Parnell Springmeyer <ixmatus@users.noreply.github.com>
2017-06-17 22:51:30 -0700

    Minor improvements for a hackage release (#3)

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
-{ mkDerivation, base, hinotify, neat-interpolation
-, optparse-applicative, optparse-generic, stdenv, stm
-, system-filepath, text, time-units, turtle
+{ mkDerivation, base, hinotify, optparse-applicative
+, optparse-generic, stdenv, stm, system-filepath, text, time-units
+, turtle
 }:
 mkDerivation {
   pname = "fswait";
-  version = "0.1.0.0";
+  version = "1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base hinotify neat-interpolation optparse-applicative
-    optparse-generic stm system-filepath text time-units turtle
+    base hinotify optparse-applicative optparse-generic stm
+    system-filepath text time-units turtle
   ];
   homepage = "https://github.com/ixmatus/fswait";
   description = "Wait and observe events on the filesystem for a path, with a timeout";
